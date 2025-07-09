72d00c7562853f7e0bd0276a91ee1d522a4ffe5c
Gabriel Gómez de la Torre <gabriel.gomezdelatorre@hotmail.com>
2025-07-09 11:48:14 -0500

    Update httpebpf.py

--- demo/httpebpf.py
+++ demo/httpebpf.py
@@ -1,81 +1,276 @@
+#!/usr/bin/python
+#
+#Bertrone Matteo - Polytechnic of Turin
+#November 2015
+#
+#eBPF application that parses HTTP packets
+#and extracts (and prints on screen) the URL contained in the GET/POST request.
+#
+#eBPF program http_filter is used as SOCKET_FILTER attached to eth0 interface.
+#only packet of type ip and tcp containing HTTP GET/POST are returned to userspace, others dropped
+#
+#python script uses bcc BPF Compiler Collection by iovisor (https://github.com/iovisor/bcc)
+#and prints on stdout the first line of the HTTP GET/POST request containing the url
+
+from __future__ import print_function
 from bcc import BPF
+from sys import argv
 
-bpf_program = """
+import sys
+import socket
+import os
+
+#args
+def usage():
+    print("USAGE: %s [-i <if_name>]" % argv[0])
+    print("")
+    print("Try '%s -h' for more options." % argv[0])
+    exit()
+
+#help
+def help():
+    print("USAGE: %s [-i <if_name>]" % argv[0])
+    print("")
+    print("optional arguments:")
+    print("   -h                       print this help")
+    print("   -i if_name               select interface if_name. Default is eth0")
+    print("")
+    print("examples:")
+    print("    http-parse              # bind socket to eth0")
+    print("    http-parse -i wlan0     # bind socket to wlan0")
+    exit()
+
+#arguments
+interface="eth0"
+
+if len(argv) == 2:
+  if str(argv[1]) == '-h':
+    help()
+  else:
+    usage()
+
+if len(argv) == 3:
+  if str(argv[1]) == '-i':
+    interface = argv[2]
+  else:
+    usage()
+
+if len(argv) > 3:
+  usage()
+
+print ("binding socket to '%s'" % interface)
+
+bpf_text="""
 #include <uapi/linux/ptrace.h>
-#include <linux/net.h>
+#include <net/sock.h>
+#include <bcc/proto.h>
 
-struct val_t {
-    void *buf;
-};
+#define IP_TCP  6
+#define ETH_HLEN 14
 
-struct data_t {
-    u32 pid;
-    u64 ts;
-    u32 len;
-    char comm[TASK_COMM_LEN];
-    char buf[256];
-};
+/*eBPF program.
+  Filter IP and TCP packets, having payload not empty
+  and containing "HTTP", "GET", "POST" ... as first bytes of payload
+  if the program is loaded as PROG_TYPE_SOCKET_FILTER
+  and attached to a socket
+  return  0 -> DROP the packet
+  return -1 -> KEEP the packet and return it to user space (userspace can read it from the socket_fd )
+*/
+int http_filter(struct __sk_buff *skb) {
 
-BPF_HASH(active_reads, u64, struct val_t);
-BPF_PERF_OUTPUT(events);
+        u8 *cursor = 0;
 
-int kprobe__sys_recvfrom(struct pt_regs *ctx, int fd, void __user *ubuf, size_t size,
-                         unsigned flags, struct sockaddr __user *addr, int __user *addrlen) {
-    u64 pid_tgid = bpf_get_current_pid_tgid();
+        struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));
+        //filter IP packets (ethernet type = 0x0800)
+        if (!(ethernet->type == 0x0800)) {
+                goto DROP;
+        }
 
-    struct val_t val = {};
-    val.buf = ubuf;
+        struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
+        //filter TCP packets (ip next protocol = 0x06)
+        if (ip->nextp != IP_TCP) {
+                goto DROP;
+        }
 
-    active_reads.update(&pid_tgid, &val);
-    return 0;
-}
+        u32  tcp_header_length = 0;
+        u32  ip_header_length = 0;
+        u32  payload_offset = 0;
+        u32  payload_length = 0;
 
-int kretprobe__sys_recvfrom(struct pt_regs *ctx) {
-    u64 pid_tgid = bpf_get_current_pid_tgid();
-    struct val_t *valp = active_reads.lookup(&pid_tgid);
-    if (valp == 0) {
-        return 0; // missed entry
-    }
+        //calculate ip header length
+        //value to multiply * 4
+        //e.g. ip->hlen = 5 ; IP Header Length = 5 x 4 byte = 20 byte
+        ip_header_length = ip->hlen << 2;    //SHL 2 -> *4 multiply
 
-    int ret = PT_REGS_RC(ctx);
-    if (ret <= 0) {
-        active_reads.delete(&pid_tgid);
+        //check ip header length against minimum
+        if (ip_header_length < sizeof(*ip)) {
+                goto DROP;
+        }
+
+        //shift cursor forward for dynamic ip header size
+        void *_ = cursor_advance(cursor, (ip_header_length-sizeof(*ip)));
+
+        struct tcp_t *tcp = cursor_advance(cursor, sizeof(*tcp));
+
+        //calculate tcp header length
+        //value to multiply *4
+        //e.g. tcp->offset = 5 ; TCP Header Length = 5 x 4 byte = 20 byte
+        tcp_header_length = tcp->offset << 2; //SHL 2 -> *4 multiply
+
+        //calculate payload offset and length
+        payload_offset = ETH_HLEN + ip_header_length + tcp_header_length;
+        payload_length = ip->tlen - ip_header_length - tcp_header_length;
+
+        //http://stackoverflow.com/questions/25047905/http-request-minimum-size-in-bytes
+        //minimum length of http request is always geater than 7 bytes
+        //avoid invalid access memory
+        //include empty payload
+        if(payload_length < 7) {
+                goto DROP;
+        }
+
+        //load first 7 byte of payload into p (payload_array)
+        //direct access to skb not allowed
+        unsigned long p[7];
+        int i = 0;
+        for (i = 0; i < 7; i++) {
+                p[i] = load_byte(skb, payload_offset + i);
+        }
+
+        //find a match with an HTTP message
+        //HTTP
+        if ((p[0] == 'H') && (p[1] == 'T') && (p[2] == 'T') && (p[3] == 'P')) {
+                goto KEEP;
+        }
+        //GET
+        if ((p[0] == 'G') && (p[1] == 'E') && (p[2] == 'T')) {
+                goto KEEP;
+        }
+        //POST
+        if ((p[0] == 'P') && (p[1] == 'O') && (p[2] == 'S') && (p[3] == 'T')) {
+                goto KEEP;
+        }
+        //PUT
+        if ((p[0] == 'P') && (p[1] == 'U') && (p[2] == 'T')) {
+                goto KEEP;
+        }
+        //DELETE
+        if ((p[0] == 'D') && (p[1] == 'E') && (p[2] == 'L') && (p[3] == 'E') && (p[4] == 'T') && (p[5] == 'E')) {
+                goto KEEP;
+        }
+        //HEAD
+        if ((p[0] == 'H') && (p[1] == 'E') && (p[2] == 'A') && (p[3] == 'D')) {
+                goto KEEP;
+        }
+
+        //no HTTP match
+        goto KEEP;
+
+        //keep the packet and send it to userspace returning -1
+        KEEP:
+        return -1;
+
+        //drop the packet returning 0
+        DROP:
         return 0;
-    }
 
-    struct data_t data = {};
-    data.pid = pid_tgid >> 32;
-    data.ts = bpf_ktime_get_ns();
-    data.len = ret;
-    bpf_get_current_comm(&data.comm, sizeof(data.comm));
-
-    size_t bytes_to_read = ret;
-    if (bytes_to_read > sizeof(data.buf)) {
-        bytes_to_read = sizeof(data.buf);
-    }
-    bpf_probe_read_user(&data.buf, bytes_to_read, valp->buf);
-
-    events.perf_submit(ctx, &data, sizeof(data));
-
-    active_reads.delete(&pid_tgid);
-    return 0;
 }
 """
+# initialize BPF - load source code from http-parse-simple.c
+#bpf = BPF(src_file = "http-parse-simple.c",debug = 0)
+bpf = BPF(text=bpf_text, debug = 0)
 
-b = BPF(text=bpf_program)
+#load eBPF program http_filter of type SOCKET_FILTER into the kernel eBPF vm
+#more info about eBPF program types
+#http://man7.org/linux/man-pages/man2/bpf.2.html
+function_http_filter = bpf.load_func("http_filter", BPF.SOCKET_FILTER)
 
-def print_event(cpu, data, size):
-    event = b["events"].event(data)
-    print(f"\nPID {event.pid} COMM {event.comm.decode()} LEN {event.len}")
-    print("Payload:")
-    print(event.buf[:event.len].decode(errors='replace'))
+#create raw socket, bind it to interface
+#attach bpf program to socket created
+BPF.attach_raw_socket(function_http_filter, interface)
 
-print("Attaching probes to sys_recvfrom... (Ctrl-C to stop)")
+#get file descriptor of the socket previously created inside BPF.attach_raw_socket
+socket_fd = function_http_filter.sock
 
-b["events"].open_perf_buffer(print_event)
+#create python socket object, from the file descriptor
+sock = socket.fromfd(socket_fd,socket.PF_PACKET,socket.SOCK_RAW,socket.IPPROTO_IP)
+#set it as blocking socket
+sock.setblocking(True)
 
-try:
-    while True:
-        b.perf_buffer_poll()
-except KeyboardInterrupt:
-    print("Detaching...")
+while 1:
+  #retrieve raw packet from socket
+  packet_str = os.read(socket_fd,2048)
+
+  #DEBUG - print raw packet in hex format
+  #packet_hex = toHex(packet_str)
+  #print ("%s" % packet_hex)
+
+  #convert packet into bytearray
+  packet_bytearray = bytearray(packet_str)
+
+  #ethernet header length
+  ETH_HLEN = 14
+
+  #IP HEADER
+  #https://tools.ietf.org/html/rfc791
+  # 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # |Version|  IHL  |Type of Service|          Total Length         |
+  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  #
+  #IHL : Internet Header Length is the length of the internet header
+  #value to multiply * 4 byte
+  #e.g. IHL = 5 ; IP Header Length = 5 * 4 byte = 20 byte
+  #
+  #Total length: This 16-bit field defines the entire packet size,
+  #including header and data, in bytes.
+
+  #calculate packet total length
+  total_length = packet_bytearray[ETH_HLEN + 2]               #load MSB
+  total_length = total_length << 8                            #shift MSB
+  total_length = total_length + packet_bytearray[ETH_HLEN+3]  #add LSB
+
+  #calculate ip header length
+  ip_header_length = packet_bytearray[ETH_HLEN]               #load Byte
+  ip_header_length = ip_header_length & 0x0F                  #mask bits 0..3
+  ip_header_length = ip_header_length << 2                    #shift to obtain length
+
+  #TCP HEADER
+  #https://www.rfc-editor.org/rfc/rfc793.txt
+  #  12              13              14              15
+  #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  # |  Data |           |U|A|P|R|S|F|                               |
+  # | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+  # |       |           |G|K|H|T|N|N|                               |
+  # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  #
+  #Data Offset: This indicates where the data begins.
+  #The TCP header is an integral number of 32 bits long.
+  #value to multiply * 4 byte
+  #e.g. DataOffset = 5 ; TCP Header Length = 5 * 4 byte = 20 byte
+
+  #calculate tcp header length
+  tcp_header_length = packet_bytearray[ETH_HLEN + ip_header_length + 12]  #load Byte
+  tcp_header_length = tcp_header_length & 0xF0                            #mask bit 4..7
+  tcp_header_length = tcp_header_length >> 2                              #SHR 4 ; SHL 2 -> SHR 2
+
+  #calculate payload offset
+  payload_offset = ETH_HLEN + ip_header_length + tcp_header_length
+
+  #print first line of the HTTP GET/POST request
+  #line ends with 0xOD 0xOA (\r\n)
+  #(if we want to print all the header print until \r\n\r\n)
+  print(packet_bytearray[payload_offset:])
+  #for i in range (payload_offset,len(packet_bytearray)-1):
+  #  if (packet_bytearray[i]== 0x0A):
+  #    if (packet_bytearray[i-1] == 0x0D):
+  #        if (packet_bytearray[i-2]== 0x0A):
+  #            if (packet_bytearray[i-3] == 0x0D):
+  #                  print(bytes(packet_bytearray[payload_offset:]))
+  #                  break
+
+    #print ("%c" % chr(packet_bytearray[i]), end = "")
+    #print ("%c" % packet_bytearray[i], end = "")
+    #print(bytes(packet_bytearray[i]))
+  print("")
